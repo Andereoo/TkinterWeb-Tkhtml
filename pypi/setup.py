470072e2ab40cb2265ff75a814d9859d30057c18
Andereoo <jx99zz@gmail.com>
2025-04-09 19:07:27 -0400

    Split TkinterWeb into TkinterWeb and TkinterWeb-Tkhtml and move source code and binaries for TkinterWeb-Tkhtml here

---
+++ pypi/setup.py
@@ -0,0 +1,29 @@
+import pathlib, platform
+from setuptools import setup, find_namespace_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name="tkinterweb-tkhtml",
+    version="1.0",
+    description="HTML/CSS viewer for Tkinter",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Andereoo/TkinterWeb-Tkhtml",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: Freeware",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+      ],
+    keywords="tkinter, Tkinter, tkhtml, Tkhtml, Tk, HTML, CSS, webbrowser",
+    packages=find_namespace_packages(include=["tkinterweb_tkhtml", "tkinterweb_tkhtml.*"]),
+    include_package_data=True,
+
+)
