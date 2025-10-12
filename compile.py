f88353fd00ace634f35fbc21c03cb54e02d95ef4
Andereoo <jx99zz@gmail.com>
2025-10-12 06:40:47 -0400

    Update compile script

--- compile.py
+++ compile.py
@@ -1,6 +1,6 @@
 ### Script to automatically compile Tkhtml
 ### Sample usage: python3 compile.py configure -w /usr/local/tcl9/bin/tclsh9.0
-### Copyright (c) 2025 Andereoo
+### Copyright (c) 2025 Andrew Clarke
 
 import tkinter
 import os, glob, subprocess, re, sys, argparse, shutil, platform
