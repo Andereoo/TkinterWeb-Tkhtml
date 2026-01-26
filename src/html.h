0e4224f79197eb00c6a5d197506fd2e24bd96d86
Zamy846692 <133600041+Zamy846692@users.noreply.github.com>
2026-01-26 00:56:03 +0000

    Moved all HtmlFloat functions and one struct from html.h to htmllayout.h

--- src/html.h
+++ src/html.h
@@ -116,7 +116,6 @@ typedef struct HtmlAttributes HtmlAttributes;
 typedef struct HtmlTokenMap HtmlTokenMap;
 typedef struct HtmlCanvas HtmlCanvas;
 typedef struct HtmlCanvasItem HtmlCanvasItem;
-typedef struct HtmlFloatList HtmlFloatList;
 typedef struct HtmlPropertyCache HtmlPropertyCache;
 typedef struct HtmlNodeReplacement HtmlNodeReplacement;
 typedef struct HtmlCallback HtmlCallback;
@@ -813,17 +812,6 @@ void HtmlWidgetOverflowBox(HtmlTree *, HtmlNode *, int *, int *, int *, int *);
 HtmlTokenMap *HtmlMarkup(int);
 CONST char * HtmlMarkupName(int);
 char * HtmlMarkupArg(HtmlAttributes *, CONST char *, char *);
-
-void HtmlFloatListAdd(HtmlFloatList*, int, int, int, int);
-HtmlFloatList *HtmlFloatListNew();
-void HtmlFloatListDelete(HtmlFloatList*);
-int HtmlFloatListPlace(HtmlFloatList*, int, int, int, int);
-int HtmlFloatListClear(HtmlFloatList*, int, int);
-int HtmlFloatListClearTop(HtmlFloatList*, int);
-void HtmlFloatListNormalize(HtmlFloatList*, int, int);
-void HtmlFloatListMargins(HtmlFloatList*, int, int, int *, int *);
-void HtmlFloatListLog(HtmlTree *, CONST char *, CONST char *, HtmlFloatList *);
-int HtmlFloatListIsConstant(HtmlFloatList*, int, int);
 
 HtmlPropertyCache * HtmlNewPropertyCache();
 void HtmlSetPropertyCache(HtmlPropertyCache *, int, CssProperty *);
