06babe42b493b5f2eecd3e5449e7c33af6e58608
Zamy846692 <133600041+Zamy846692@users.noreply.github.com>
2026-02-02 00:12:21 +0000

    Refactored CSS search function

--- src/csssearch.c
+++ src/csssearch.c
@@ -156,7 +156,7 @@ HtmlCssSearch(
 
     /* Search only descendants of this node (NULL means search whole tree) */
     HtmlNode *pSearchRoot = 0;
-    int eMode = SEARCH_MODE_ALL;
+    u8 eMode = SEARCH_MODE_ALL;
     int iIndex = 0;
 
     int iArg;
@@ -260,15 +260,13 @@ HtmlCssSearch(
     switch (eMode) {
         case SEARCH_MODE_ALL: {
             Tcl_Obj *pRet = Tcl_NewObj();
-            int ii;
-            for(ii = 0; ii < pCache->nNode; ii++){
-                Tcl_Obj *pCmd = HtmlNodeCommand(pTree, pCache->apNode[ii]);
+            for(int i = 0; i < pCache->nNode; i++){
+                Tcl_Obj *pCmd = HtmlNodeCommand(pTree, pCache->apNode[i]);
                 Tcl_ListObjAppendElement(interp, pRet, pCmd);
             }
             Tcl_SetObjResult(interp, pRet);
             break;
         }
-
         case SEARCH_MODE_LENGTH:
             Tcl_SetObjResult(interp, Tcl_NewIntObj(pCache->nNode));
             break;
