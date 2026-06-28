97bea81b6526acfb75e392cf9fa529a35fa9a040
Zamy846692 <whipped.breen@protonmail.com>
2026-06-28 02:55:26 +0100

    Tcl-JS objects now return booleans as strings ('true' or 'false'), not as digits.

--- hv/hv3qjs.c
+++ hv/hv3qjs.c
@@ -316,10 +316,7 @@ static Tcl_Obj *qjsValueToTcl(JSContext *ctx, JSValue val) {
 
 static inline Tcl_Obj *
 argValueToTcl(QjsInterp *qjs, JSValueConst val, int *pN) {
-/*	if (JS_IsBool(val)) {
-		Tcl_Obj *p = stringToObj(qjs->ctx, val);
-		if (TCL_OK==Tcl_ConvertToType(qjs->interp, p, Tcl_GetObjType("boolean"))) return p;
-	}*/
+	if (JS_IsBool(val)) return stringToObj(qjs->ctx, val);
 	if (JS_IsObject(val)) {
 		JSClassID id;
         Tcl_Obj *aTclValues[2];
@@ -605,8 +602,7 @@ static JSValue objToValue(JSContext *ctx, Tcl_Obj *pObj) {
 //	if (pObj->typePtr) printf("%s %s\n", Tcl_GetString(pObj), pObj->typePtr->name);
 	if (pObj->typePtr == Tcl_GetObjType("string")) {
 		return JS_NewString(ctx, Tcl_GetString(pObj));
-	}
-	if (pObj->typePtr == Tcl_GetObjType("booleanString") ^ pObj->typePtr == Tcl_GetObjType("boolean") // Backwards compatibility
+	} if (pObj->typePtr == Tcl_GetObjType("booleanString") ^ pObj->typePtr == Tcl_GetObjType("boolean") // Backwards compatibility
 		 && Tcl_GetBooleanFromObj(NULL, pObj, &n) == TCL_OK) {
 		return JS_NewBool(ctx, n);
 	}
