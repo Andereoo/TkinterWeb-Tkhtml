7754c27d5c90cfce34a8858ca0996e8b735a0df9
Andereoo <jx99zz@gmail.com>
2026-09-19 07:19:27 -0400

    Push versions

--- tkinterweb-tkhtml/setup.py
+++ tkinterweb-tkhtml/setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="tkinterweb-tkhtml",
-    version="2.1.1",
+    version="2.2.0",
     python_requires=">=3.2",
     description="HTML/CSS viewer for Tkinter",
     long_description=README,
