978b4f6fb6d6059ebdcbf65e63d00501ba0c8b32
Andereoo <jx99zz@gmail.com>
2026-09-19 19:25:19 -0400

    Troubleshooting

--- compile.py
+++ compile.py
@@ -40,7 +40,8 @@ tkhtml_file = None
 
 root = tkinter.Tcl()
 paths = root.exprstring('$auto_path').split()
-print(paths)
+if "/usr/lib" in paths:
+    paths.remove("/usr/lib")
 version = str(tkinter.TkVersion)
 paths = list(set(paths))
 paths.sort(key=len)
@@ -197,14 +198,11 @@ elif (not os.path.exists(BUILD_PATH) and mode == "build") or mode == "configure"
         print("Done!", flush=True)
 
     print("\nSearching for Tcl/Tk configuration files...", flush=True)
-    print(paths)
-    if with_tclsh:
-        print(run_command([with_tclsh, GET_PATHS_PATH], capture_output=True))
+
     def search():
         global tclConfig_paths, tkConfig_paths, valid_tclConfig_paths, valid_tkConfig_paths
-        
+
         for path in paths: 
-            print(path, flush=True)
             if os.path.exists(path):
                 may_continue = True
                 if may_continue:
